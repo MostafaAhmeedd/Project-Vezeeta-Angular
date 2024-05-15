36b1c11b0d6d9e5bf787a6a71476c5e2f44a4ee9
Mostafa Ahmed <mostafa.ahmed55201@gmail.com>
2024-05-15 17:35:05 +0300

    New-Service-Part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewServicesComponent } from './Components/new-services/new-services.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NewServicesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
