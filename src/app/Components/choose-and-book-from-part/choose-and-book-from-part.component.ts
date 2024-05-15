41ca8ac61d552769e07a6fc00bb2a36ec9b55b3e
Sandraiskanderr <sandraiskanderr@gmail.com>
2024-05-16 00:59:22 +0300

    second two components

---
+++ src/app/Components/choose-and-book-from-part/choose-and-book-from-part.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-choose-and-book-from-part',
+  standalone: true,
+  imports: [],
+  templateUrl: './choose-and-book-from-part.component.html',
+  styleUrl: './choose-and-book-from-part.component.css'
+})
+export class ChooseAndBookFromPartComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MarketingPartComponent } from './Components/marketing-part/marketing-part.component';
 import { DownloadPartComponent } from './Components/download-part/download-part.component';
+import { ChooseAndBookFromPartComponent } from './Components/choose-and-book-from-part/choose-and-book-from-part.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MarketingPartComponent, DownloadPartComponent],
+  imports: [RouterOutlet, MarketingPartComponent, DownloadPartComponent, ChooseAndBookFromPartComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
