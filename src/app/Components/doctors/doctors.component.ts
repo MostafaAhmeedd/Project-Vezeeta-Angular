6963685ce8614060984442e950c4156008a1c8df
MostafaAhmeedd <mostafa.ahmed55201@gmail.com>
2024-05-18 16:26:08 +0300

    Modification-5

--- src/app/Components/doctors/doctors.component.ts
+++ src/app/Components/doctors/doctors.component.ts
@@ -2,7 +2,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { DoctorServiceService } from '../../Services/doctor-service.service';
-
 @Component({
   selector: 'app-doctors',
   standalone: true,
@@ -13,8 +12,6 @@ import { DoctorServiceService } from '../../Services/doctor-service.service';
   schemas:[CUSTOM_ELEMENTS_SCHEMA],
 })
 export class DoctorsComponent {
-  // doctor:{name: string, title:string, rating: number , ratingCount:number ,specialization:string,location:string,fees:number,waitingTime:number,phone:string} = 
-  // { name: '', title: '', rating: 0,ratingCount:0 ,specialization:'',location:'',fees:0,waitingTime:0,phone:""};
   doctors: any = [];
 
 constructor(private router: Router ,myRoute: ActivatedRoute, private Dservice:DoctorServiceService ) {
