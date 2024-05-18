2af2fa15bb8ee74dc1041ee31aa4141bcd1cce14
MostafaAhmeedd <mostafa.ahmed55201@gmail.com>
2024-05-18 15:01:10 +0300

    Modification-3

--- src/app/components/backgrounds/backgrounds.component.ts
+++ src/app/components/backgrounds/backgrounds.component.ts
@@ -1,39 +1,34 @@
-import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
-
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-backgrounds',
   standalone: true,
   imports: [RouterModule],
   templateUrl: './backgrounds.component.html',
-  styleUrl: './backgrounds.component.css'
+  styleUrls: ['./backgrounds.component.css']
 })
-export class BackgroundsComponent {
+export class BackgroundsComponent implements OnInit {
+  ngOnInit(): void {
+    const backgroundImage = document.getElementById('backgroundImage') as HTMLImageElement;
+    const images = [
+        "assets/images/background1.jpg",
+        "assets/images/background2.jpg",
+        "assets/images/background3.jpg",
+        "assets/images/background4.jpg"
+    ];
 
-}
+    let currentImageIndex = 0;
 
+    function switchBackground() {
+        backgroundImage.src = images[currentImageIndex];
+        backgroundImage.classList.remove('active');
+        void backgroundImage.offsetWidth; 
+        backgroundImage.classList.add('active');
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+    }
 
-document.addEventListener("DOMContentLoaded", () => {
-  const backgroundImage = document.getElementById('backgroundImage') as HTMLImageElement;
-  const images = [
-      "assets/images/background1.jpg",
-      "assets/images/background2.jpg",
-      "assets/images/background3.jpg",
-      "assets/images/background4.jpg"
-  ];
-
-  let currentImageIndex = 0;
-
-  function switchBackground() {
-      backgroundImage.src = images[currentImageIndex];
-      backgroundImage.classList.remove('active'); 
-      void backgroundImage.offsetWidth; 
-      backgroundImage.classList.add('active'); 
-      currentImageIndex = (currentImageIndex + 1) % images.length; 
+    switchBackground(); 
+    setInterval(switchBackground, 4000); 
   }
-
-  switchBackground(); 
-  setInterval(switchBackground, 4000); 
-});
-
+}
